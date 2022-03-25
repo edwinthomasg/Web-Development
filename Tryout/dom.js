dc3a6f5dc24a70dce7b99ae597b53fc68d27c28e
Edwin <edwinthomasg29@gmail.com>
2022-03-25 14:00:42 +0530

    commit

--- Tryout/dom.js
+++ Tryout/dom.js
@@ -1,19 +1,32 @@
-function clickEvent() {
-    /*var name = document.getElementById("name").value;
-    var pass = document.getElementById("psw").value;
-    if(name == pass)
-    alert("both matches");
-    else
-    alert("both not matches");
+// function clickEvent() {
+//     /*var name = document.getElementById("name").value;
+//     var pass = document.getElementById("psw").value;
+//     if(name == pass)
+//     alert("both matches");
+//     else
+//     alert("both not matches");
 
-    var male = document.getElementById("m");
-    var female = document.getElementById("f");
-    if(male.checked == true)
-    document.write(male.value);
-    else if(female.checked == true)
-    document.write(female.value);*/
+//     var male = document.getElementById("m");
+//     var female = document.getElementById("f");
+//     if(male.checked == true)
+//     document.write(male.value);
+//     else if(female.checked == true)
+//     document.write(female.value);*/
 
-    var select = document.getElementById("select");
-    alert(select.options[select.selectedIndex].value);
+//     var select = document.getElementById("select");
+//     alert(select.options[select.selectedIndex].value);
     
+// }
+user = {
+    name:"vasi",
+    age:21,
+    show:function(){
+        console.log(this.name)
+        fun = () => {
+            console.log(this.name)
+        }
+        
+        fun()
+    }
 }
+user.show()
